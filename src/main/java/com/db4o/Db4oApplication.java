dc2d592471f4f150d513ac2dbec1831260c1eacd
Iyare I. Diagboya <toolz.skillz@gmail.com>
2025-03-23 18:12:43 +0100

    genesis

---
+++ src/main/java/com/db4o/Db4oApplication.java
@@ -0,0 +1,15 @@
+package com.db4o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db4oApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Db4oApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/db4o/Db4oApplicationTests.java
@@ -0,0 +1,15 @@
+//package com.db4o;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Db4oApplicationTests
+//{
+//
+//    @Test
+//    void contextLoads()
+//    {
+//    }
+//
+//}
